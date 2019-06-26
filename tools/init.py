375ef341df85a2f53b3da509fe2f6c8bf7dbd30d
Kerry <flaght@vip.qq.com>
2019-06-26 19:49:13 +0800

    Update init.py

--- tools/init.py
+++ tools/init.py
@@ -1,5 +1,5 @@
 from ultron.config import config_setting
 
-config_setting.set_queue(qtype='redis', host='180.166.26.82',
+config_setting.set_queue(qtype='redis', host='172.17.0.1',
                         port=4430, pwd='12345678dx', db=1)
 config_setting.update()
