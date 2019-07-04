dbdf549d48e6d951f48762bf7e9a3d9ddc4efd9c
Kerry <kerry@miglab.com>
2019-07-04 09:32:39 +0800

    新增例子

--- ultron/setting.py
+++ ultron/setting.py
@@ -1,3 +1,3 @@
 import os
-setting_info='{"queue": {"type": "redis", "host": "127.0.0.1", "port": 1234, "pwd": "123456", "db": 1}}'
+setting_info='{"queue": {"type": "queue", "host": "180.166.26.82", "port": 4430, "pwd": "12345678dx", "db": 1}}'
 file_name = os.path.join(os.getcwd(), __file__)
