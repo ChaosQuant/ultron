168eb5e7d4c589a4ac5f028241cdf10c4647b9b0
kerry <kerry@miglab.com>
2019-08-29 09:07:50 +0800

    剔除无效文件

--- example/Example 4 - Queue /client.py
+++
@@ -1,8 +0,0 @@
-from stm.task1 import add
-from stm.task2 import multiply
-
-tm = add.delay(4,2)
-tm.ready()
-print(tm.get())
-tp = multiply.delay(6,2)
-print(tp.get())
--- example/Example 4 - Queue /stm/task1.py
+++
@@ -1,6 +0,0 @@
-import time
-from . import app
-@app.task(ignore_result=True)
-def add(x, y):
-    time.sleep(2)
-    return x + y
