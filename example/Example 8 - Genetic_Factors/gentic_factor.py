8530e15630ec3c8efa1ee1bb257eacbed40dc633
Kerry <kerry@miglab.com>
2019-10-07 00:25:24 +0800

    修改遗传bug

---
+++ example/Example 8 - Genetic_Factors/gentic_factor.py
@@ -0,0 +1,32 @@
+import pickle,itertools,sys,pdb
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import graphviz
+from ultron.factor.genetic.geneticist.genetic import Gentic
+
+import warnings
+warnings.filterwarnings("ignore")
+
+## IC 方法
+def websim_weighted(factor_data, total_data, factor_sets):
+    factor_data = factor_data.copy()
+    factor_data = factor_data.reset_index().sort_values(['trade_date','code'])
+    #此处ret放的位置不对，仅用于测试例
+    factor_data = factor_data.fillna(0)
+    score = np.corrcoef(factor_data['transformed'].values, total_data.sort_values(['trade_date','code'])['ret'].values)[0,1]
+    return abs(score)
+
+
+with open('factor_data.pkl','rb') as file2:
+    total_data = pickle.load(file2)
+    
+factor_sets = [i for i in list(set(total_data.columns)) if i not in ['trade_date','code','ret']]
+gentic = Gentic(population_size=10, tournament_size = 3, n_jobs = 4, stopping_criteria=100,
+                factor_sets = factor_sets,
+                fitness=websim_weighted)
+
+gentic.train(total_data=total_data)
+result = gentic._run_details
+pdb.set_trace()
+print('------')
---
+++ ultron/utilities/jobs.py
@@ -0,0 +1,45 @@
+import numbers
+
+import numpy as np
+from joblib import cpu_count
+
+def _get_n_jobs(n_jobs):
+    """Get number of jobs for the computation.
+
+    This function reimplements the logic of joblib to determine the actual
+    number of jobs depending on the cpu count. If -1 all CPUs are used.
+    If 1 is given, no parallel computing code is used at all, which is useful
+    for debugging. For n_jobs below -1, (n_cpus + 1 + n_jobs) are used.
+    Thus for n_jobs = -2, all CPUs but one are used.
+
+    Parameters
+    ----------
+    n_jobs : int
+        Number of jobs stated in joblib convention.
+
+    Returns
+    -------
+    n_jobs : int
+        The actual number of jobs as positive integer.
+
+    """
+    if n_jobs < 0:
+        return max(cpu_count() + 1 + n_jobs, 1)
+    elif n_jobs == 0:
+        raise ValueError('Parameter n_jobs == 0 has no meaning.')
+    else:
+        return n_jobs
+
+
+def partition_estimators(n_estimators, n_jobs):
+    """Private function used to partition estimators between jobs."""
+    # Compute the number of jobs
+    n_jobs = min(_get_n_jobs(n_jobs), n_estimators)
+
+    # Partition estimators between jobs
+    n_estimators_per_job = (n_estimators // n_jobs) * np.ones(n_jobs,
+                                                              dtype=np.int)
+    n_estimators_per_job[:n_estimators % n_jobs] += 1
+    starts = np.cumsum(n_estimators_per_job)
+
+    return n_jobs, n_estimators_per_job.tolist(), [0] + starts.tolist()
